75ddd6dd873b102a31f1b7c2d9d40fcd87279be7
jianghai <jianghai1007@gmail.com>
2016-05-26 20:05:15 +0800

    update

--- index.js
+++ index.js
@@ -8,53 +8,50 @@
 
 var fs = require('fs')
 
+var recurse = function(executer) {
+  return executer.apply(null, [].slice.call(arguments, 1).concat(executer))
+}
+
 var getCataloguesStr = function(catalogues) {
   var result = ''
-
-  function catalogueRender(list, indent) {
+  recurse(function(list, indent, executer) {
     list.forEach(function(item) {
       var title = item.orders.join('.') + ' ' + item.title
       var href = item.orders.join('') + '-' + item.title
       href = href.toLowerCase()
       href = href.replace(/\s/g, '-')
       result += Array(indent + 1).join(' ') + '* [' + title + '](#' + href + ')\r\n'
-      item.children && catalogueRender(item.children, indent + 2)
-    }, this)
-  }
-  catalogueRender(catalogues, 0)
+      item.children && executer(item.children, indent + 2)
+    })
+  }, catalogues, 0)
   return result
 }
 
-var getOrderedFileContentAndCatalogues = function(fileContent) {
+var parse = function(getCataloguesStr, fileContent) {
   var orders = []
   var catalogues = []
-  var replacer = function(match, level, title) {
+  fileContent = fileContent.replace(/(#{2,})\s*[\d\.\s]*(.*)/g, function(match, level, title) {
     var start = level.length - 2
     if (orders.length !== start + 1) {
       orders.length = start + 1
       orders[start] || (orders[start] = 0)
     }
     orders[start]++
-
+    
     var temp = catalogues
-    var i = 0
-    while (orders[++i]) {
-      temp = temp[orders[i - 1] - 1]
+    orders.slice(1).forEach(function(order, i) {
+      temp = temp[orders[i] - 1]
       temp.children || (temp.children = [])
       temp = temp.children
-    }
-
+    })
     temp.push({
       orders: orders.concat(),
       title: title
     })
+    
     return level + ' ' + orders.join('.') + ' ' + title
-  }
-  return [fileContent.replace(/(#{2,})\s*[\d\.\s]*(.*)/g, replacer), catalogues]
-}
-
-var getFileContentWithCatalogues = function(getCataloguesStr, res) {
-  return res[0].replace(/\n[\s\S]*?##/, '\n\n' + getCataloguesStr(res[1]) + '\n##')
+  })
+  return fileContent.replace(/\n[\s\S]*?##/, '\n\n' + getCataloguesStr(catalogues) + '\n##')
 }
 
 var readFile = function(fs, file) {
@@ -80,8 +77,7 @@ var writeFile = function(fs, dist, fileContent) {
  */
 var generate = function(file, dist) {
   readFile(fs, file)
-    .then(getOrderedFileContentAndCatalogues)
-    .then(getFileContentWithCatalogues.bind(null, getCataloguesStr))
+    .then(parse.bind(null, getCataloguesStr))
     .then(writeFile.bind(null, fs, dist || file))
     .catch(function(reason) { console.log(reason) })
 }
